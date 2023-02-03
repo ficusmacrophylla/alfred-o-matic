ec82d3f618b2a3a9c8ca1a4be540b69b0ad44e1e
ficusmacrophylla <matteo.menzolini@gmail.com>
2023-02-03 08:57:56 +0100

    + pizza model + pizza service + pizza view page (with errors)

--- Alfredo/Services/PizzaService.cs
+++ Alfredo/Services/PizzaService.cs
@@ -1,6 +1,45 @@
-﻿namespace Alfredo.Services
+﻿using Alfredo.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Alfredo.Services
 {
-    public class PizzaService
+    public static class PizzaService
     {
+        static List<Pizza> Pizzas { get; }
+        static int nextId = 3;
+        static PizzaService()
+        {
+            Pizzas = new List<Pizza>
+            {
+                new Pizza { Id = 1, Name = "Classic Italian", Price = 20.00M, Size = PizzaSize.Large, IsGlutenFree = false },
+                new Pizza { Id = 1, Name = "Veggie", Price = 15.00M, Size = PizzaSize.Small, IsGlutenFree = true }
+            };
+        }
+
+        public static List<Pizza> GetAll() => Pizzas;
+        public static Pizza? Get(int id) => Pizzas.FirstOrDefault(x => x.Id == id);
+
+        public static void Add (Pizza pizza)
+        {
+            pizza.Id = nextId++;
+            Pizzas.Add(pizza);
+        }
+
+        public static void Delete(int id) 
+        {
+            var pizza = Get(id);
+            if (pizza is null)
+                return;
+            Pizzas.Remove(pizza);
+        }
+
+        public static void Update(Pizza pizza)
+        {
+            var index = Pizzas.FindIndex( p => p.Id == pizza.Id);
+            if (index == -1)
+                return;
+
+            Pizzas[index] = pizza;
+        }
     }
 }
